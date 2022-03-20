8d9318e82dbcd80c9c595e43f814d01389f3074c
andreeapurta <andreea.purta@gmail.com>
2022-03-21 00:59:10 +0200

    folder structure fix

---
+++ eShop.StateStore.LocalStorage/ShoppingCartStateStore.cs
@@ -0,0 +1,36 @@
+﻿using eShop.UseCases.Interfaces.StateStore;
+using eShop.UseCases.Interfaces.UI;
+using System.Threading.Tasks;
+
+namespace eShop.StateStore.LocalStorage
+{
+    public class ShoppingCartStateStore : StateStoreBase, IShoppingCartStateStore
+    {
+        private readonly IShoppingCart shoppingCart;
+
+        public ShoppingCartStateStore(IShoppingCart shoppingCart)
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public async Task<int> GetLineItemsCount()
+        {
+            var order = await shoppingCart.GetOrderAsync();
+            if (order != null && order.LineItems != null && order.LineItems.Count > 0)
+            {
+                return order.LineItems.Count;
+            }
+            return 0;
+        }
+
+        public void LineItemsCountUpdated()
+        {
+            base.BroadCastStateChange();
+        }
+
+        public void ProductQuantityUpdated()
+        {
+            base.BroadCastStateChange();
+        }
+    }
+}
---
+++ eShop.StateStore.LocalStorage/StateStoreBase.cs
@@ -0,0 +1,28 @@
+﻿using eShop.UseCases.Interfaces.StateStore;
+using System;
+
+namespace eShop.StateStore.LocalStorage
+{
+    public class StateStoreBase : IStateStore
+    {
+        protected Action listeners;
+
+        public void AddStateChangeListeners(Action listener)
+        {
+            listeners += listener;
+        }
+
+        public void BroadCastStateChange()
+        {
+            if (listeners != null)
+            {
+                listeners.Invoke();
+            }
+        }
+
+        public void RemoveStateChangeListeners(Action listener)
+        {
+            listeners -= listener;
+        }
+    }
+}
